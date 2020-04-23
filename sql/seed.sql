81ea5c14fce44681a3ba7fa53cb7e032372e6e32
Dyana Wu <dyanawu@gmail.com>
2020-04-23 16:49:01 +0800

    Add test SQL stuff

---
+++ sql/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO test_table (content) VALUES
+('THIS IS A TEST TABLE PLEASE CHANGE YOUR VALUES'),
+('Some times I stay up cashing in my bad luck'),
+('Sometimes I wish I''d just fall down'),
+('I''m too school for cool'),
+('I''m not a perfect person');
